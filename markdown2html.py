ea35bd88d220d505ae9bcace92ea75a2ea049d79
NekodaMushi <study.fabien@gmail.com>
2023-07-31 16:26:41 +0200

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
 """function takes argument 2 strings"""
+if __name__ == "__main__":
+    import os
+    import sys
 
-import os
-import sys
+    if len(sys.argv) < 2:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        exit(1)
 
-if len(sys.argv) < 2:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-    exit(1)
+    if not os.path.exists(sys.argv[1]):
+        sys.stderr.write("Missing " + sys.argv[1] + "\n")
+        exit(1)
 
-if not os.path.exists(sys.argv[1]):
-    sys.stderr.write("Missing " + sys.argv[1] + "\n")
-    exit(1)
-
-exit(0)
+    exit(0)
