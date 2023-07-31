164078380df41838f323fe8b8c83bb7ad068c81d
NekodaMushi <study.fabien@gmail.com>
2023-07-31 16:39:14 +0200

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-"""function takes argument 2 strings"""
+"""function parsing Headings Markdown syntax for generating HTML"""
+
+import os
+import sys
+
 if __name__ == "__main__":
     import os
     import sys
@@ -12,5 +16,20 @@ if __name__ == "__main__":
     if not os.path.exists(sys.argv[1]):
         sys.stderr.write("Missing " + sys.argv[1] + "\n")
         exit(1)
+
+    with open(sys.argv[1], "r") as f_mark:
+        lines = f_mark.readlines()
+
+    html_content = []
+    for line in lines:
+        if line.startswith("#"):
+            Hlevel = line.count("#")
+            textContent = line[Hlevel:].strip()
+            html_content.append(f"<h{Hlevel}>{textContent}</h{Hlevel}>")
+
+    html_version = "\n".join(html_content)
+
+    with open(sys.argv[2], "w") as f_html:
+        f_html.write(html_version)
 
     exit(0)
